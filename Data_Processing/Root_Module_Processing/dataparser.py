a7ce6850b31de0a5f249f0fe1a438131cf068d95
clocktop_dell <clocker.kyle@gmail.com>
2020-12-05 13:02:23 -0800

    Small change to dataparser to read all files

--- Data_Processing/Root_Module_Processing/dataparser.py
+++ Data_Processing/Root_Module_Processing/dataparser.py
@@ -1,18 +1,16 @@
 import os
-
-
-with open('./raw_data/2chan.txt') as infile, open('./processed_data/2chan_processed.txt','w') as outfile:
-    data = infile.read()
-    data = data.replace("[","")
-    data = data.replace("]","")
-    data = data.replace(" ","")
-    data = data.replace("\n",",")
-    outfile.write(data)
-    outfile.seek(0, os.SEEK_END)
-    outfile.seek(outfile.tell() - 1,0)
-    outfile.truncate()
-
-
-
-
-
+path = './raw_data'
+files = os.listdir(path)
+for file in files:
+    with open('./raw_data/'+file) as infile, open('./processed_data/'+file[0:len(file)-4]+'_processed.txt','w') as outfile:
+        data = infile.read()
+        data = data.replace("\n[]","")
+        data = data.replace("[","")
+        data = data.replace("]","")
+        data = data.replace(" ","")
+        data = data.replace("\n",",")
+        outfile.write(data)
+        outfile.seek(0, os.SEEK_END)
+        outfile.seek(outfile.tell() - 1,0)
+        outfile.truncate()
+    print(file)
