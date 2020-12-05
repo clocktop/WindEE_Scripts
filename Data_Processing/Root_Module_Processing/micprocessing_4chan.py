b92bd73e632437138d7d8d2819a29c4dce094894
clocktop_dell <clocker.kyle@gmail.com>
2020-12-05 12:53:10 -0800

    reflected ploting changes from 2chanto 4chan

--- Data_Processing/Root_Module_Processing/micprocessing_4chan.py
+++ Data_Processing/Root_Module_Processing/micprocessing_4chan.py
@@ -45,12 +45,31 @@ print(f'Took {toc-tic:0.4f} seconds')
 
 # %%
 sample_rate = 100000
-n = 2096
+n = len(d0)
+x = np.linspace(0, n/sample_rate, n)
+
+fig1, ax1 = plt.subplots(figsize = [10,5])
+ax1.plot(x,d0)
+
+ax1_ins = ax1.inset_axes([0.5, 0.5, 0.47, 0.47])
+ax1_ins.plot(x[4000:6000], d0[4000:6000])
+ax1_ins.set_xticklabels('') 
+ax1_ins.set_yticklabels('') 
+
+ax1.indicate_inset_zoom(ax1_ins)
+
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('ADC Code')
+ax1.set_title('Sample Recording')
+plt.show()
+# %%
+sample_rate = 100000
+n = 8192
 win = np.hamming(n)
-samp_sig = d0[0:2096] * win
+samp_sig = d0[0:8192]-np.round(np.mean(d0[0:8192])) * win
 yf = rfft(samp_sig)
 xf = rfftfreq(n,1/sample_rate)
-ref = 4095
+ref = 4096
 yf_mag = np.abs(yf) * 2 / np.sum(win)
 
 yf_dbfs = 20 * np.log10(yf_mag/ref)
@@ -60,6 +79,25 @@ fig2, ax2 = plt.subplots(figsize = [10,5])
 ax2.semilogx(xf, yf_dbfs)
 ax2.grid(True)
 ax2.set_xlabel('Frequency [Hz]')
-ax2.set_ylabel('Amplitude [dB]')
+ax2.set_ylabel('Amplitude [dBfs]')
 ax2.set_title('FFT of Sample Signal')
+
+peakf = xf[np.argmax(yf)]
+peakamp = yf_dbfs[np.argmax(yf)]
+xdisp, ydisp = ax2.transData.transform_point((peakf, peakamp))
+bbox = dict(boxstyle ='round',fc='0.8')
+arrowprops = dict(
+    arrowstyle = '->',
+    connectionstyle = 'angle,angleA=0,angleB=90,rad=10'
+)
+offset = 72
+
+ax2.annotate(
+    'Peakf = %.1f Hz \n Peaka = %.1f dBfs'%(peakf,peakamp),
+    (peakf,peakamp),
+    xytext=(offset,-offset/2),
+    textcoords='offset points',
+    bbox = bbox,
+    arrowprops = arrowprops
+)
 # %%
